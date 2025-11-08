90d00fe2d8a5b288569fae60f39547cc38cae4f7
tgorlachov-bu <tgorlachov@binghamton.edu>
2025-11-08 16:59:48 -0500

    lizard spin in reaction to "Windy" Area2Ds

--- scene/lizard.gd
+++ scene/lizard.gd
@@ -2,6 +2,7 @@ extends Sprite2D
 
 
 const TONGUE_OFFSET := Vector2(0, -8)
+const SPIN_TOTAL := 2.5 * PI
 
 @export var extend_tween_duration := 0.7
 @export var retract_tween_duration := 0.2
@@ -9,16 +10,17 @@ const TONGUE_OFFSET := Vector2(0, -8)
 @export var tongue_length := 32.0
 @export var steppable := false
 
-@onready var detect_tongue: Area2D = $DetectTongue
+@onready var detector: Area2D = $Detector
 @onready var tongue: Area2D = $Tongue
 @onready var tongue_line: Line2D = $TongueLine
 
 var extend_tween: Tween
 var retract_tween: Tween
+var wind_spin_tween: Tween
 var extending := false
 static var extend_count := 0
 
-func rebuild_tweens() -> void:
+func rebuild_tongue_tweens() -> void:
 	extend_tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 	extend_tween.stop()
 	extend_tween.tween_property(tongue, "position", TONGUE_OFFSET + Vector2(0, -tongue_length), extend_tween_duration)
@@ -29,13 +31,19 @@ func rebuild_tweens() -> void:
 	retract_tween.tween_property(tongue, "position", TONGUE_OFFSET, retract_tween_duration)
 	retract_tween.tween_callback(retract_done)
 
+func rebuild_spin_tween() -> void:
+	wind_spin_tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	wind_spin_tween.stop()
+	wind_spin_tween.tween_property(self, "rotation", rotation + SPIN_TOTAL, 1)
+	wind_spin_tween.tween_callback(rebuild_spin_tween)
+
 func retract_done() -> void:
 	tongue.monitorable = true
 	extend_count -= 1
 	tongue_line.set_point_position(1, Vector2.ZERO)
 	extending = false
 
-	rebuild_tweens()
+	rebuild_tongue_tweens()
 
 
 func extend_done() -> void:
@@ -43,13 +51,13 @@ func extend_done() -> void:
 	await get_tree().create_timer(hold_duration).timeout
 	retract_tween.play()
 
-func tongue_detected(a: Area2D):
-	if !a.is_in_group("LizardTongue") || a == tongue || extending:
-		return
-
-	extend_tween.play()
-	extending = true
-	extend_count += 1
+func detector_entered(a: Area2D):
+	if a.is_in_group("LizardTongue") && a != tongue && !extending:
+		extend_tween.play()
+		extending = true
+		extend_count += 1
+	elif a.is_in_group("Windy") && !wind_spin_tween.is_running():
+		wind_spin_tween.play()
 
 # Only steppable lizards connect this
 func body_entered(b: Node2D) -> void:
@@ -61,14 +69,15 @@ func body_entered(b: Node2D) -> void:
 	extend_count += 1
 
 func _ready() -> void:
-	rebuild_tweens()
+	rebuild_tongue_tweens()
+	rebuild_spin_tween()
 
 	if steppable:
-		texture = texture.duplicate()
+		texture = texture.duplicate() # texture is shared among all lizards, only change this one via dupe
 		(texture as AtlasTexture).region.position = Vector2(16, 0)
-		detect_tongue.body_entered.connect(body_entered)
+		detector.body_entered.connect(body_entered)
 
-	detect_tongue.area_entered.connect(tongue_detected)
+	detector.area_entered.connect(detector_entered)
 
 func _process(_delta: float) -> void:
 	if extend_tween.is_running():
