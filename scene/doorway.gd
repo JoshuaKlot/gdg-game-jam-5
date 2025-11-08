032919587e4e8a46864e3e434968244589dcb1b8
tgorlachov-bu <tgorlachov@binghamton.edu>
2025-11-08 12:56:35 -0500

    smol wizard, doorway impl

---
+++ scene/doorway.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+@export var load_room: int
+
+func _body_entered(b: Node2D) -> void:
+	if b.is_in_group("Player"):
+		_G.request_room_change.emit(_G.room_to_scene[load_room])
+
+func _ready() -> void:
+	body_entered.connect(_body_entered)
