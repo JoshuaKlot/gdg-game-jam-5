a0007631d61f5f4a3c7d2bb826400882953c27f1
Juklok <107051800+JoshuaKlot@users.noreply.github.com>
2025-11-09 13:05:38 -0500

    Rock(in progress)

--- scene/rock.gd
+++ scene/rock.gd
@@ -1,41 +1,45 @@
-extends Area2D
+extends RigidBody2D
 
 @export var tile_size=16
-@export var speed: float = 0.35
-var tween: Tween
-
+@export var speed: float = 10
+var direction=Vector2.DOWN
+var movingTowards
+var moving=false
 func move(distance,moveTo):
 	print("moving from "+str(position)+" to "+str(position+(distance*moveTo)))
-	tween=create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
+	movingTowards=position+(distance*moveTo)
+	moving=true
 	
-	tween.tween_property(self,"position",position+(distance*moveTo),speed)
 	
 func _physics_process(delta: float) -> void:
-	if tween:
-		if tween.is_running():
-			$AnimatedSprite2D.play("moving")
-		else:
-			$AnimatedSprite2D.play("default")
+	if moving:
+		linear_velocity=linear_velocity.move_toward(movingTowards,speed)
+		$AnimatedSprite2D.play("moving")
+		if position.is_equal_approx(movingTowards) or linear_velocity==Vector2.ZERO:
+			moving=false
+	else:
+		linear_velocity=Vector2.ZERO
+		$AnimatedSprite2D.play("default")
+	
+	
 
-func _on_area_entered(area: Area2D) -> void:
+func _on_area_2d_area_entered(area: Area2D) -> void:
 	if(area.is_in_group("Windy")):
-		var direction=Vector2.DOWN
-		var hitFrom=area.position-position
-		if abs(hitFrom.x)>abs(hitFrom.y):
-			if hitFrom.x>0:
-				direction=Vector2.LEFT
-			else:
-				direction=Vector2.RIGHT
-		else:
-			if hitFrom.y>0:
-				direction=Vector2.UP
-			else:
-				direction=Vector2.DOWN
+
+		
+		
+		direction=_G.throwingDirection
+		#if abs(hitFrom.x)>abs(hitFrom.y):
+			#if hitFrom.x>0:
+				#direction=Vector2.LEFT
+			#else:
+				#direction=Vector2.RIGHT
+		#else:
+			#if hitFrom.y>0:
+				#direction=Vector2.UP
+			#else:
+				#direction=Vector2.DOWN
 		if area.thrown:
 			move(tile_size*3,direction)
 		else:
 			move(tile_size,direction)
-	
-
-
-	
