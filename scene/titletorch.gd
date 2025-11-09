6b62a0b9df4928679864b46504e9e13080c82a0b
tgorlachov-bu <tgorlachov@binghamton.edu>
2025-11-09 17:05:44 -0500

    dont ever add titletorch to dict

--- scene/titletorch.gd
+++ scene/titletorch.gd
@@ -3,20 +3,14 @@ extends AnimatedSprite2D
 var on_fire := false:
 	set(x):
 		on_fire = x
-		Darkness.get_node("ColorRect").material["shader_parameter/lights_on"][id] = int(x)
-@onready var id : int = _G.torch_ids.get_or_add(position, _G.torch_ids.size() + 1)
+		Darkness.get_node("ColorRect").material["shader_parameter/lights_on"][0] = int(x)
 @onready var player = get_tree().get_first_node_in_group("Player")
 
 func _ready() -> void:
-	if _G.torch_puzzle_lit.get_or_add(position, id == 1):
-		call_deferred("enflame")
 	z_index = floori(global_position.y)
 
 func enflame() -> void:
 	if not on_fire:
-		_G.torch_puzzle_lit.set(position, true)
-		_G.torch_puzzle_changed.emit()
-
 		Darkness.get_node("ColorRect").material["shader_parameter/size"] = 0
 		for i in 40:
 			Darkness.get_node("ColorRect").material["shader_parameter/size"] = i / 40. * (80 + 5 * sin(2*Time.get_unix_time_from_system()))
@@ -33,8 +27,6 @@ func extinguish() -> void:
 			Darkness.get_node("ColorRect").material["shader_parameter/size"] = (39-i) / 40. * s
 			await get_tree().process_frame
 		for i in 9: Darkness.get_node("ColorRect").material["shader_parameter/lights_on"][1 + i] = 0
-		_G.torch_puzzle_lit.set(position, false)
-		_G.torch_puzzle_changed.emit()
 		play("default")
 		on_fire = false
 
@@ -48,4 +40,4 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 
 func _process(delta):
 	if on_fire:
-		Darkness.get_node("ColorRect").material["shader_parameter/lights"][id] = global_position - player.get_node("Camera2D").get_screen_center_position() + Vector2(256/2, 192/2)
+		Darkness.get_node("ColorRect").material["shader_parameter/lights"][0] = global_position - player.get_node("Camera2D").get_screen_center_position() + Vector2(256/2, 192/2)
